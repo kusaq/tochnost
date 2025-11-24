6ae87e437cd0f11e22e9b5c53344633b94e0fa5d
maxim <terror20007@mail.ru>
2025-11-24 20:39:31 +0300

    add: expand rail

--- src/infra/timescale_db/models/rail.py
+++ src/infra/timescale_db/models/rail.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 from sqlalchemy import String, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from infra.timescale_db.mixins import CreateTimestampMixin
 from infra.timescale_db.models.base import Base
 
 
-class Rail(Base):
+class Rail(Base, CreateTimestampMixin):
     __tablename__ = "rail"
 
     rail_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
@@ -13,6 +16,8 @@ class Rail(Base):
     object_name: Mapped[str | None] = mapped_column(String(255), nullable=True)
     fastening_type: Mapped[str | None] = mapped_column(String(255), nullable=True)
     sleepers: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    start_time: Mapped[datetime | None] = mapped_column(nullable=True)
+    end_time: Mapped[datetime | None] = mapped_column(nullable=True)
 
     sensor1_records = relationship("Sensor1", back_populates="rail")
     sensor2_records = relationship("Sensor2", back_populates="rail")
